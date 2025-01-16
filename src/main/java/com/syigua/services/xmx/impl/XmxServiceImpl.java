cd8fde684d00d20e772df1130d90bd96194cf17c
huangfuliang <1416011240@qq.com>
2025-01-16 11:18:55 +0800

    修复微信主被发送关系

--- src/main/java/com/syigua/services/xmx/impl/XmxServiceImpl.java
+++ src/main/java/com/syigua/services/xmx/impl/XmxServiceImpl.java
@@ -1,5 +1,6 @@
 package com.syigua.services.xmx.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.syigua.dto.ModelResponse;
 import com.syigua.po.SygPromptPO;
 import com.syigua.services.models.QwenModelsService;
@@ -7,7 +8,6 @@ import com.syigua.services.models.SygPromptService;
 import com.syigua.services.xmx.XmxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,7 +31,9 @@ public class XmxServiceImpl implements XmxService {
         map.put("zw_content", content);
         String smxPrompt = wrapPrompt(map, sygPromptPO.getPrompt());
         ModelResponse response = qwenModelsService.getXms(smxPrompt);
-        return response.getResponse();
+        JSONObject choices = JSONObject.parseObject(response.getResponse());
+        JSONObject choice = choices.getJSONArray("choices").getJSONObject(0);
+        return choice.getJSONObject("message").getString("content");
     }
 
     private String wrapPrompt(Map<String, String> map, final String sygPrompt) {
